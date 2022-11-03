d50f52a1d937fa949b8089504cae4c25d0673cd4
LucasMartins-Dev <lucasmartinsms.dev@gmail.com>
2022-11-03 01:42:11 -0300

    mod

--- scripts.js
+++ scripts.js
@@ -20,12 +20,6 @@ function finalizarquizz(){
 }
 
 
-
-    
-
-
-
-
 function exibirQuiz(quiz) {
     for(let i = 0; i < quiz.data.questions.length ; i++){}
     for(let a = 0; a < quiz.data.levels.length ; a++){}
