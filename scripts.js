60687a465ae544562af7d4166ec88f40b6fb3fb9
LucasMartins-Dev <lucasmartinsms.dev@gmail.com>
2022-11-03 01:13:10 -0300

    botoes funcionando

---
+++ scripts.js
@@ -0,0 +1,24 @@
+function criarperguntas(){
+    let começo = document.querySelector('.criar-inicio')
+    começo.classList.remove('exibir')
+    let perguntas = document.querySelector('.criar-perguntas')
+    perguntas.classList.add('exibir')
+}
+
+function criarniveis(){
+    let perguntas = document.querySelector('.criar-perguntas')
+    perguntas.classList.remove('exibir')
+    let niveis = document.querySelector('.criar-niveis')
+    niveis.classList.add('exibir')
+}
+
+function finalizarquizz(){
+    let niveis = document.querySelector('.criar-niveis')
+    niveis.classList.remove('exibir')
+    let finalizar = document.querySelector('.finalizar-quizz')
+    finalizar.classList.add('exibir')
+}
+
+
+
+    
