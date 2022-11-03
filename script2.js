4daf213996e3b380399e4fb378cef0c371063410
MarmoW <115847012+MarmoW@users.noreply.github.com>
2022-11-03 02:15:04 -0300

    Html e Js pagina do quiz

--- script2.js
+++ script2.js
@@ -1,21 +1,26 @@
-let perguntas = document.querySelector(".todasrespostas");
+
 
 pegarQuiz();
 
 function pegarQuiz(){
     const pegando = axios.get("https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes/1");
-    pegando.then(exibirQuiz());
+    pegando.then(exibirQuiz);
 }
 
 function exibirQuiz(quiz) {
-    console.log(quiz.data);
-    perguntas.innerHTML = "";
-    for(let i = 0; i < quiz.data.questions.answers.length ; i++){ 
-        perguntas.innerHTML += `<div class="respostas"><img src="${quiz.data.questions.answers[i].image}" width="330px" height="175px"><p class="${quiz.data.questions.answers[i].text}"> Gatíneo </p></div>`;
+    console.log(quiz);
+    //perguntas.innerHTML = "";
+    let container = document.querySelector(".containerquiz");
+    container.innerHTML += `<img src="${quiz.data.imagem}" class="mainimgquiz">`;
+    for(let b = 0; b < quiz.data.questions.length ; b++) {
+    container.innerHTML += `<div class="pergunta p${b}"><div class="textopergunta"><p>${quiz.data.questions[b].title}</p></div><div class="todasrespostas r${b}"></div></div>`;
+        for(let i = 0; i < quiz.data.questions[b].answers.length ; i++){ 
+            let perguntas = document.querySelector(`.todasrespostas .r${b}`);
+            perguntas.innerHTML += `<div class="respostas"><img src="${quiz.data.questions[b].answers[i].image}" width="330px" height="175px"><p class="${quiz.data.questions[b].answers[i].text}"> Gatíneo </p></div>`;
     }
     for(let a = 0; a < quiz.data.levels.length ; a++){}
 
-
+ }
 }
 
-//testando commit
+
